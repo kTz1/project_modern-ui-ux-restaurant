54c98afdb522e6d769b61a6838ed4281d3535f06
kTz1 <catalin_simon90@yahoo.com>
2022-01-29 17:42:32 +0200

    added Header container and subHeading component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { MdOutlineRestaurantMenu } from 'react-icons/md';
 
-import images from '../../constants/images';
+import { images } from '../../constants';
 
 import './Navbar.css';
 
