1b13548eed737ca7c84103023e38a67199702a26
kTz1 <catalin_simon90@yahoo.com>
2022-01-29 16:08:57 +0200

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdOutlineResturantMenu } from 'react-icons/md';
+
+import images from '../../constants/images';
+
+import './Navbar.css';
+
+const Navbar = () => (
+  <nav className="app__navbar">
+    Navbar
+  </nav>
+);
+
+export default Navbar;
